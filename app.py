f532b4d294dc0518a21925d2cd4b85cf3b5d6b7e
Noelle Zahara <noellezahara@gmail.com>
2025-02-25 09:27:43 -0700

    Add 404 page

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def inject_user():
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('home.html', title='YTPMV Sample Database')
+    return render_template('404.html', title='YTPMV Sample Database')
 
 
 @app.route('/')
