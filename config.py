bf1fd7d31d45d551c01a4b808b3d4452a43cc92a
Noelle <44215890+NoelleTGS@users.noreply.github.com>
2025-09-19 19:28:44 -0600

    Add email verification (#46)
    * Add verified status to user in `models.py`
    
    * Add mail variables to config
    
    * Create `mail.py`
    
    * Add endpoint for email verification
    
    * Add email verification when registering
    
    * Change email salt method to be more secure
    
    * Stylize verification email
    
    * Add proper website for email verification
    
    ---------
    
    Co-authored-by: IoIxD <alphaproject217@gmail.com>

--- config.py
+++ config.py
@@ -11,3 +11,10 @@ MAX_CONTENT_LENGTH = MB_UPLOAD_LIMIT * 10 * 1000 * 1000
 SAMPLES_PER_PAGE = settings["samples_per_page"]
 ALLOWED_UPLOAD_EXTENSIONS = settings["allowed_upload_extensions"]
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+MAIL_SERVER = settings["mail_server"] or "smtp.example.com"
+MAIL_PORT = int(settings["mail_port"] or 587)
+MAIL_USE_TLS = settings["mail_use_tls"] or True  # Use TLS for most SMTP providers
+MAIL_USE_SSL = settings["mail_use_ssl"] or False  # Use SSL only if required (never enable both)
+MAIL_USERNAME = settings["MAIL_USERNAME"] or "your_email@example.com"
+MAIL_PASSWORD = settings["MAIL_PASSWORD"] or "hackme"
---
+++ mail.py
@@ -0,0 +1,41 @@
+from flask_mail import Message, Mail
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
+from env import FLASK_SECRET_KEY
+import secrets
+
+mail = Mail()
+
+s = URLSafeTimedSerializer(FLASK_SECRET_KEY)
+email_hash = secrets.token_hex(4096)
+
+def generate_token(email):
+    return s.dumps(email, salt=email_hash)
+
+def confirm_token(token, expiration=86400):
+    try:
+        return s.loads(token, salt=email_hash, max_age=expiration)
+    except SignatureExpired:
+        return False
+    except BadSignature:
+        return False
+
+def send_verification_email(to, verify_url):
+    try:
+        msg = Message(
+            subject="Verify your account at YTPMVSD",
+            recipients=[to],
+            sender="account@ytpmvsd.com",
+            html=f"""
+            <div style="font-family: 'Noto Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
+            <table>
+                <tr align="center"><td style="padding: 0.5em;"><center><img src="https://ytpmvsd.com/static/img/logo.png"/ width="50%"></center></td></tr>
+                <tr align="center"><td style="padding: 0.5em;"><h3>Please confirm your email address to use YTPMVSD.</h3></td></tr>
+                <tr align="center"><td style="padding: 0.5em;"><a style="display: block; background: #324ca8; padding: 1em; color: white; font-weight: bold; border-radius: 5px; width: 5em; text-decoration: none;" href="{verify_url}">Verify</a></td></tr>
+                <tr align="center"><td>This link expires in 24 hours.</td></tr>
+                <tr align="center"><td>Do not click this link if you didn't sign up for this site.</td></tr>
+            </table>
+            """
+        )
+        mail.send(msg)
+    except Exception as e:
+        print(e)
